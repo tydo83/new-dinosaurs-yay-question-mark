1c618f7b44cc98bc9846f7f680db4362c187a03e
Colin Jaffe <balloonasaurus@gmail.com>
2020-10-19 07:11:30 -0400

    Adds all changes from solution repo.

--- main.test.js
+++ main.test.js
@@ -6,29 +6,126 @@ const {
 } = require('./main.js')
 
 
-
-
 describe('makeDino', () => {
-  it(`given a species name, a period, and a diet, returns a dinosaur object with those values, as well as a default status of 'not extinct'`, () => {
-    const expectedDino = {
+  it(`given a species name, a period, a diet, and an extinct status of true, returns a dinosaur object with those values`, () => {
+    const expectedDino1 = {
       species: 'Eoraptor',
       period: 'Triassic',
       carnivore: true,
-      extinct: false
+      extinct: true
     }
 
-    expect(makeDino('Eoraptor', 'Triassic', true)).toEqual(expectedDino)
-  });
-
-  it(`allows us to create a dinosaur with status extinct`, () => {
-    const expectedDino = {
-      species: 'Brachiosaurus',
-      period: 'Jurassic',
+    const expectedDino2 = {
+      species: 'Stegosaurus',
+      period: 'Cretaceous',
       carnivore: false,
       extinct: true
     }
 
-    expect(makeDino('Brachiosaurus', 'Jurassic', false, true)).toEqual(expectedDino)
+    const dino1 = makeDino('Eoraptor', 'Triassic', true, true);
+    const dino2 = makeDino('Stegosaurus', 'Cretaceous', false, true);
+
+    expect(dino1).toEqual(expectedDino1)
+    expect(dino2).toEqual(expectedDino2)
+  });
+
+  it(`allows us to create a dinosaur that is not extinct`, () => {
+    const expectedDino1 = {
+      species: 'Brachiosaurus',
+      period: 'Jurassic',
+      carnivore: false,
+      extinct: false,
+    }
+
+    const expectedDino2 = {
+      species: 'Plesiosaur',
+      period: 'Triassic',
+      carnivore: true,
+      extinct: false,
+    }
+
+    const dino1 = makeDino('Brachiosaurus', 'Jurassic', false, false)
+    const dino2 = makeDino('Plesiosaur', 'Triassic', true, false)
+
+    expect(dino1).toEqual(expectedDino1)
+    expect(dino2).toEqual(expectedDino2)
+  })
+
+  it(`if no parameter is passed for extinct, default to false`, () => {
+    const expectedDino1 = {
+      species: 'Pteranodon',
+      period: 'Jurassic',
+      carnivore: true,
+      extinct: false,
+    }
+
+    const expectedDino2 = {
+      species: 'Dimetrodon',
+      period: 'Cretaceous',
+      carnivore: true,
+      extinct: false,
+    }
+
+    const dino1 = makeDino('Pteranodon', 'Jurassic', true)
+    const dino2 = makeDino('Dimetrodon', 'Cretaceous', true)
+
+    expect(dino1).toEqual(expectedDino1)
+    expect(dino2).toEqual(expectedDino2)
+  })
+})
+
+describe('makeExtinct', () => {
+  it(`returns a new dinosaur with its extinct set to true`, () => {
+    const dino1 = {
+      species: 'Brachiosaurus',
+      period: 'Jurassic',
+      carnivore: false,
+      extinct: false
+    };
+
+    const extinctDino1 = {
+      species: 'Brachiosaurus',
+      period: 'Jurassic',
+      carnivore: false,
+      extinct: true
+    };
+
+    const dino2 = {
+      species: 'T-Rex',
+      period: 'Cretaceous',
+      carnivore: true,
+      extinct: false
+    };
+
+    const extinctDino2 = {
+      species: 'T-Rex',
+      period: 'Cretaceous',
+      carnivore: true,
+      extinct: true
+    };
+
+    expect(makeExtinct(dino1)).toEqual(extinctDino1);
+    expect(makeExtinct(dino2)).toEqual(extinctDino2);
+  })
+
+  it(`does not mutate the original object`, () => {
+    const dinoTemplate = {
+      species: 'T-Rex',
+      period: 'Cretaceous',
+      carnivore: true,
+      extinct: false
+    };
+
+    const dino = {
+      species: 'T-Rex',
+      period: 'Cretaceous',
+      carnivore: true,
+      extinct: false
+    };
+
+    makeExtinct(dino)
+    
+    expect(dino).toEqual(dinoTemplate);
   })
 })
 
@@ -97,6 +194,7 @@ describe('truncateSpecies', () => {
       carnivore: false,
       extinct: true
     };
+
     expect(truncateSpecies(dino)).toEqual(truncatedDino);
   })
 
@@ -127,61 +225,6 @@ describe('truncateSpecies', () => {
     };
 
     truncateSpecies(dino)
-    
-    expect(dino).toEqual(dinoTemplate);
-  })
-})
-
-describe('makeExtinct', () => {
-  it(`returns a new dinosaur with its extinct set to true`, () => {
-    const dino1 = {
-      species: 'Brachiosaurus',
-      period: 'Jurassic',
-      carnivore: false,
-      extinct: false
-    };
-
-    const extinctDino1 = {
-      species: 'Brachiosaurus',
-      period: 'Jurassic',
-      carnivore: false,
-      extinct: true
-    };
-
-    const dino2 = {
-      species: 'T-Rex',
-      period: 'Cretaceous',
-      carnivore: true,
-      extinct: false
-    };
-
-    const extinctDino2 = {
-      species: 'T-Rex',
-      period: 'Cretaceous',
-      carnivore: true,
-      extinct: true
-    };
-
-    expect(makeExtinct(dino1)).toEqual(extinctDino1);
-    expect(makeExtinct(dino2)).toEqual(extinctDino2);
-  })
-
-  it(`does not mutate the original object`, () => {
-    const dinoTemplate = {
-      species: 'T-Rex',
-      period: 'Cretaceous',
-      carnivore: true,
-      extinct: true
-    };
-
-    const dino = {
-      species: 'T-Rex',
-      period: 'Cretaceous',
-      carnivore: true,
-      extinct: true
-    };
-
-    makeExtinct(dino)
     
     expect(dino).toEqual(dinoTemplate);
   })
