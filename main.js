74cb08caddd17d6e4ff02c2fee660c912a032333
Timothy Do <tydo83@gmail.com>
2020-10-19 13:29:42 -0400

    done first version

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 // Your code below.
+const makeDino = function(newSpecies, newPeriod, newCarnivore, newExtinct) {
+  const newDino = {species: newSpecies, period: newPeriod, carnivore: newCarnivore, extinct: newExtinct};
+  if(newExtinct === undefined) {newDino.extinct = false};
+  return newDino;
+}
 
+const makeExtinct = function(dino) {
+  const newDino =  makeDino(dino.species, dino.period, dino.carnivore, dino.extinct);
+  newDino.extinct = true;
+  return newDino;
+}
 
+const truncateSpecies = function(dino) {
+  const newDino = makeDino(dino.species, dino.period, dino.carnivore, dino.extinct);
+  if(newDino.species.length > 7) {newDino.species = `${newDino.species.slice(0, 7)}...`};
+  return newDino;
+}
 
-
+const makeSingular = function(dino) {
+  const newDino = makeDino(dino.species, dino.period, dino.carnivore, dino.extinct);
+  if(newDino.species.endsWith('us')) {newDino.species = newDino.species.slice(0, newDino.species.indexOf('us'))}
+  return newDino;
+}
 
 // Our code below; DO NOT TOUCH!
 if (typeof makeDino === 'undefined') {
